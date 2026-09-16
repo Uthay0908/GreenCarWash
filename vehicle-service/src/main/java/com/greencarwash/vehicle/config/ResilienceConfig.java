928e3292d72adb640feb84956ee5db7518814df8
SATHYA P <sathyapm1006@gmail.com>
2026-09-16 22:35:24 +0530

    Delete vehicle-service directory

--- vehicle-service/src/main/java/com/greencarwash/vehicle/config/ResilienceConfig.java
+++
@@ -1,7 +0,0 @@
-package com.greencarwash.vehicle.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ResilienceConfig {
-}
--- vehicle-service/src/main/java/com/greencarwash/vehicle/exception/ResourceNotFoundException.java
+++
@@ -1,5 +0,0 @@
-package com.greencarwash.vehicle.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) { super(message); }
-}
