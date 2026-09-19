9b66206d9c71ec61ccdc86b76da0f1029e17294e
RaajeshKumaar17 <raajeshvelusamy@gmail.com>
2026-09-19 15:39:48 +0530

    Delete support-service directory

--- support-service/src/main/java/com/itransform/support/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.itransform.support.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
